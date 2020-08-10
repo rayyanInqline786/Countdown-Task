50a04098a9836f4a22b494fbdc24b7a3c09b0c29
rayyanInqline786 <69470014+rayyanInqline786@users.noreply.github.com>
2020-08-10 20:14:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var fiveSeconds = new Date().getTime() + 120000;
+var time = new Date().getTime() + 120000;
 $(document).ready(function(){
-    $('#countdown-timer').countdown(fiveSeconds, function(event) {
+    $('#countdown-timer').countdown(time, function(event) {
         $(this).html(event.strftime('%H:%M:%S'));
       }).on('finish.countdown', function() {
         $(this).hide()
