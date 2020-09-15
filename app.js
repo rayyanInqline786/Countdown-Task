7c0e036935bb20c7883da209e38504d1ad2acdd7
rayyanInqline786 <rayyan@inqline.com>
2020-09-15 11:51:43 -0400

    first commit reviewed by mustufa

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
-var time = new Date().getTime() + 120000;
+// Time remaining before class
+var remTimeC1 = new Date().getTime()+12000
+var remTimeC2 = new Date().getTime()+18000
 $(document).ready(function(){
-    $('#countdown-timer').countdown(time, function(event) {
-        $(this).html(event.strftime('%H:%M:%S'));
-      }).on('finish.countdown', function() {
-        $(this).hide()
-        $("#boomRoom").show();
-    });
+    $('#countdown').countdown(`${remTimeC1}`,{elapse:true})
+.on('update.countdown', function(event) {
+  // Passed Time
+  if (event.elapsed) {
+    $(this).html(event.strftime('Class Duration Elapsed (Passed): <span>%H:%M:%S</span>'));
+     setTimeout(function(){
+      $('#countdown').countdown('stop');
+        $("#countdown").hide()
+        $(".finish-text").show()
+        $(".joinLink").bind('click', false);
+        $(".class1 a").addClass("mute")
+    },3000)
+        $(".on-going").show()
+        $(".endTime").show()
+        $("#waiting").hide()
+        $(".joinLink").show()
+    } 
+    // Time remaining before class
+    else {
+    // $(this).html(event.strftime('Class will start in: <span>%H:%M:%S</span>'),function(){});
+    $(this).html(event.strftime('Class will start in: <span>%H:%M:%S</span>'));
+  }
 })
-
+$('#countdown1').countdown(`${remTimeC2}`,{elapse:true})
+.on('update.countdown', function(event) {
+  if (event.elapsed) {
+    $(this).html(event.strftime('Class Duration Elapsed (Passed): <span>%H:%M:%S</span>'));
+     setTimeout(function(){
+      $('#countdown1').countdown('stop');
+        $("#countdown1").hide()
+        $(".finish-text1").show()
+        $(".joinLink1").bind('click', false);
+        $("a").addClass("mute1")
+    },9000)
+        $(".on-going").show()
+        $(".endTime").show()
+        $("#waiting1").hide()
+        $(".joinLink1").show()
+    } else {
+    $(this).html(event.strftime('Class will start in: <span>%H:%M:%S</span>'))
+    }
+})
+})
